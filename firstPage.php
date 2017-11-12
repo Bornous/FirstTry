2170b83b14c914a094854b0aa65760c6a4dd4683
Bornous <pavelgra77@gmail.com>
2017-11-12 13:56:18 +0100

    ...

---
+++ firstPage.php
@@ -0,0 +1,36 @@
+<?php
+
+	$jsF='"background-color:grey;color:black;"';
+	
+
+
+	echo "A więc, niechaj rozpocznie się przygoda!!!<br><br>";
+	echo "Na początek coś prostego. Kliknij na czerwoną ramkę<br>";
+	
+	echo	"<div id='redText' style='background-color:red; color:red;' onclick='myFunction()'>
+					Świeżo odkryty tekst.
+	
+	
+				</div>";
+			
+	echo "
+			<script>
+				if(document.getElementById('redText')!=0)document.getElementById('redText').onclick = function (){myFunction()};
+				function myFunction(){
+					document.getElementById('redText').style=$jsF;
+					
+				}
+			
+			</script>
+	
+	
+	
+	";
+
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	echo "Witaj, witaj - nieznajomy!!!<br><br>";
+	echo "Jeżeli wolą Twoją jest przejść dalej - uiścić opłatę godziwym byłoby zachowaniem!<br><br>";
+	
+	echo"<form action='firstPage.php' method='POST'>
+				<input type='submit' value='ZAPŁAĆ' name='mySalaryLoosers'/>
+			</form>";
+			
+	
+
+
+
+
+
+
+
+?>
